4b041ad794417f9f1bfb5e2e420d2dabcacbf370
Aidan <aidan.mellin@gmail.com>
2020-04-23 11:56:15 -0400

    first commit!

---
+++ secrets.py
@@ -0,0 +1,8 @@
+spot_user_id = 'mixmasterman'
+spotify_token = 'BQBt-53q2QBW4bcxwq5EiiDwnwzTrRBkMV3HlirhvB8te-i2eCMGKwecRWdJMd_QarNw57HhKDIrQtPGRPV0lSKgjgAD4zNOSqFQS8cL1BGNAmsxRRuGloYDOCefWZc6kjYStlS3sW_pzzY3tnVE-9Ie1o3VZSGWneb6m2g-AwhmHGGSedpRLGshMlUzT3zheNI7ghr378sxmgQyh4HgoGl3lMUb3wkMoriF'
+client_id = '1b4474a00d8e42c5b5bbe0cfc8fe2362'
+client_secret = ' 8279bffa701b4511abedb65bb9876932'
+
+"""
+https://developer.spotify.com/console/post-playlist-tracks/?playlist_id=1xhBMRrsHlsxkxPNWArUAo&position=&uris=spotify%3Atrack%3A6JgXQTdqLL1xS5qfqOGVNb%2Cspotify%3Atrack%3A4lrNKogLNNobrYMK0m4ljy
+"""
