90e811cd976c85803d4c1c155d7fa07fa679181a
Aidan <aidan.mellin@gmail.com>
2024-04-26 13:25:24 -0400

    clean up

--- secrets.py
+++
@@ -1,8 +0,0 @@
-spot_user_id = ''
-spotify_token = ''
-client_id = ''
-client_secret = ''
-
-"""
-https://developer.spotify.com/console/post-playlist-tracks/?playlist_id=1xhBMRrsHlsxkxPNWArUAo&position=&uris=spotify%3Atrack%3A6JgXQTdqLL1xS5qfqOGVNb%2Cspotify%3Atrack%3A4lrNKogLNNobrYMK0m4ljy
-"""
