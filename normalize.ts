21335d2378f7d49af3f2e312a7329e13ac19653e
Aidan <aidan.mellin@gmail.com>
2025-09-27 12:33:40 -0400

    Multi-song compare base

--- normalize.ts
+++ normalize.ts
@@ -1,12 +1,27 @@
 import type { Features, Song } from "./song"
 
+const FEATURE_LENGTH=10;
+
+const FEATURE_IDX: Record<string, number> = {
+    acousticness: 0,
+    energy: 1,
+    danceability: 2,
+    valence: 3,
+    loudness: 4,
+    speechiness: 5,
+    instrumentalness: 6,
+    liveness: 7,
+    tempo: 8,
+    popularity: 9
+};
+const FEATURE_NAME_KEYS: Array<string> = Object.keys(FEATURE_IDX); 
+
 export interface ModSong
 {
     name: string;
     id: string;
-    features: Features;
-    normalized_features: Features;
-    weighted_features: Features | null; 
+    features: Features; //Original data
+    normalized_features: Features; //Normalized between 0-1
 }
 
 /**
@@ -73,37 +88,38 @@ export function findMinAndMax(songs: Song[]): Features[]
     return [min, max]
 }
 
-function normalizeValue(value:number, min: number, max: number): number
+/**
+ * 
+ * @param value baseSong feature value to be normalized 
+ * @param min Min feature value across all songs
+ * @param max Max feature value across all songs
+ * @returns the normalized value
+ */
+function normalizeValue(value:number, min: number, max: number, baseSong: number): number
 {
-    return (value - min)/(max-min);
+    return Math.abs(baseSong - value)/(max-min);
 }
 
-export function normalizeSongFeatures(songs: Song[]): ModSong[]
-{
-    const [min, max]: Features[] = findMinAndMax(songs);
-
-    let moddedSongs: ModSong[] = [];
-
-    for(const song of songs)
-    {
-        moddedSongs.push(normalize(song, min, max));
-    }
-    return moddedSongs
-}
-
-function normalize(value: Song, min: Features, max: Features): ModSong
+/**
+ * 
+ * @param value The song to be normalized
+ * @param min Minimum feature values across song set
+ * @param max Max feature values across song set
+ * @returns the new interface including normalized values
+ */
+function normalize(value: Song, min: Features, max: Features, baseSong: Song): ModSong
 {
     let normFeat: Features = {
-        acousticness: normalizeValue(value.features.acousticness, min.acousticness, max.acousticness),
-        energy: normalizeValue(value.features.energy, min.energy, max.energy),
-        danceability: normalizeValue(value.features.danceability, min.danceability, max.danceability),
-        valence: normalizeValue(value.features.valence, min.valence, max.valence),
-        loudness: normalizeValue(value.features.loudness, min.loudness, max.loudness),
-        speechiness: normalizeValue(value.features.speechiness, min.speechiness, max.speechiness),
-        instrumentalness: normalizeValue(value.features.instrumentalness, min.instrumentalness, max.instrumentalness),
-        liveness: normalizeValue(value.features.liveness, min.liveness, max.liveness),
-        tempo: normalizeValue(value.features.tempo, min.tempo, max.tempo),
-        popularity: normalizeValue(value.features.popularity, min.popularity, max.popularity),
+        acousticness: normalizeValue(value.features.acousticness, min.acousticness, max.acousticness, baseSong.features.acousticness ),
+        energy: normalizeValue(value.features.energy, min.energy, max.energy, baseSong.features.energy ),
+        danceability: normalizeValue(value.features.danceability, min.danceability, max.danceability, baseSong.features.danceability ),
+        valence: normalizeValue(value.features.valence, min.valence, max.valence, baseSong.features.valence ),
+        loudness: normalizeValue(value.features.loudness, min.loudness, max.loudness, baseSong.features.loudness ),
+        speechiness: normalizeValue(value.features.speechiness, min.speechiness, max.speechiness, baseSong.features.speechiness ),
+        instrumentalness: normalizeValue(value.features.instrumentalness, min.instrumentalness, max.instrumentalness, baseSong.features.instrumentalness ),
+        liveness: normalizeValue(value.features.liveness, min.liveness, max.liveness, baseSong.features.liveness ),
+        tempo: normalizeValue(value.features.tempo, min.tempo, max.tempo, baseSong.features.tempo ),
+        popularity: normalizeValue(value.features.popularity, min.popularity, max.popularity, baseSong.features.popularity ),
         key_mode: null,
         genres: null
     }
@@ -112,6 +128,168 @@ function normalize(value: Song, min: Features, max: Features): ModSong
         id: value.id,
         features: value.features,
         normalized_features: normFeat,
-        weighted_features: null
     }
+}
+
+/**
+ * 
+ * @param songs All songs to be normalized for comparison
+ * @returns Array of aggregated song data
+ */
+export function normalizeSongFeatures(songs: Song[], baseSong: Song): ModSong[]
+{
+    const [min, max]: Features[] = findMinAndMax(songs);
+
+    let moddedSongs: ModSong[] = [];
+
+    for(const song of songs)
+    {
+        moddedSongs.push(normalize(song, min, max, baseSong));
+    }
+    return moddedSongs
+}
+
+export function getAverageFeatures(songs: ModSong[]): Features
+{
+    let n = songs.length;
+    let weights: Features = {
+        acousticness : 0,
+        energy: 0,
+        danceability: 0,
+        valence: 0,
+        loudness: 0,
+        speechiness: 0,
+        instrumentalness: 0,
+        liveness: 0,
+        tempo: 0,
+        popularity: 0,
+        key_mode: null,
+        genres: null
+    }
+
+    //Make a big sum per feature
+    for(const song of songs)
+    {
+        weights.acousticness += song.normalized_features.acousticness;
+        weights.energy += song.normalized_features.energy;
+        weights.danceability += song.normalized_features.danceability;
+        weights.valence += song.normalized_features.valence;
+        weights.loudness += song.normalized_features.loudness;
+        weights.speechiness += song.normalized_features.speechiness;
+        weights.instrumentalness += song.normalized_features.instrumentalness;
+        weights.liveness += song.normalized_features.liveness;
+        weights.tempo += song.normalized_features.tempo;
+        weights.popularity += song.normalized_features.popularity;
+    }
+
+    //Divide out the big sums by the length of the songs array to get average
+    weights.acousticness /= n;
+    weights.energy /= n;
+    weights.danceability /= n;
+    weights.valence /= n;
+    weights.loudness /= n;
+    weights.speechiness /= n;
+    weights.instrumentalness /= n;
+    weights.liveness /= n;
+    weights.tempo /= n;
+    weights.popularity /= n;
+
+    return weights;
+}
+
+export function getWeights(songs: ModSong[], baseSong: Features, avg: Features): Features
+{
+    let weights: Features = {
+        acousticness : 0,
+        energy: 0,
+        danceability: 0,
+        valence: 0,
+        loudness: 0,
+        speechiness: 0,
+        instrumentalness: 0,
+        liveness: 0,
+        tempo: 0,
+        popularity: 0,
+        key_mode: null,
+        genres: null
+    }
+
+    for (const song of songs)
+    {
+        // if(song.normalized_features.acousticness < (Math.abs(baseSong.normalized_features.acousticness - avg.acousticness))) weights.acousticness += 1;
+        // if(song.normalized_features.energy < (Math.abs(baseSong.normalized_features.energy - avg.energy))) weights.energy += 1;
+        // if(song.normalized_features.danceability < (Math.abs(baseSong.normalized_features.danceability - avg.danceability))) weights.danceability += 1;
+        // if(song.normalized_features.valence < (Math.abs(baseSong.normalized_features.valence - avg.valence))) weights.valence += 1;
+        // if(song.normalized_features.loudness < (Math.abs(baseSong.normalized_features.loudness - avg.loudness))) weights.loudness += 1;
+        // if(song.normalized_features.speechiness < (Math.abs(baseSong.normalized_features.speechiness - avg.speechiness))) weights.speechiness += 1;
+        // if(song.normalized_features.instrumentalness < (Math.abs(baseSong.normalized_features.instrumentalness - avg.instrumentalness))) weights.instrumentalness += 1;
+        // if(song.normalized_features.liveness < (Math.abs(baseSong.normalized_features.liveness - avg.liveness))) weights.liveness += 1;
+        // if(song.normalized_features.tempo < (Math.abs(baseSong.normalized_features.tempo - avg.tempo))) weights.tempo += 1;
+        // if(song.normalized_features.popularity < (Math.abs(baseSong.normalized_features.popularity - avg.popularity))) weights.popularity += 1;
+
+        if (1-Math.abs(baseSong.acousticness - song.normalized_features.acousticness) < avg.acousticness) weights.acousticness += 1;
+        if (1-Math.abs(baseSong.energy - song.normalized_features.energy) < avg.energy) weights.energy += 1;
+        if (1-Math.abs(baseSong.danceability - song.normalized_features.danceability) < avg.danceability) weights.danceability += 1;
+        if (1-Math.abs(baseSong.valence - song.normalized_features.valence) < avg.valence) weights.valence += 1;
+        if (1-Math.abs(baseSong.loudness -song.normalized_features.loudness) < avg.loudness) weights.loudness += 1;
+        if (1-Math.abs(baseSong.speechiness - song.normalized_features.speechiness) < avg.speechiness) weights.speechiness += 1;
+        if (1-Math.abs(baseSong.instrumentalness - song.normalized_features.instrumentalness) < avg.instrumentalness) weights.instrumentalness += 1;
+        if (1-Math.abs(baseSong.liveness - song.normalized_features.liveness) < avg.liveness) weights.liveness += 1;
+        if (1-Math.abs(baseSong.tempo - song.normalized_features.tempo) < avg.tempo) weights.tempo += 1;
+        if (1-Math.abs(baseSong.popularity - song.normalized_features.popularity) < avg.popularity) weights.popularity += 1;
+    }
+
+    return weights;
+}
+
+export function generateBaseCompareFeatures(songs: ModSong[]): Features
+{
+    // Pass in a list of the base compare songs, we want to determine the general features to then compare against.
+    // Something like Gowers, but something more like
+    /**
+     * Goal: Weighted average of values in Features.
+     * 
+     * Change it to compare against the elements in the base song set, find the closest values and assign from there?
+     */
+    let compare: Features = {
+        acousticness : 0,
+        energy: 0,
+        danceability: 0,
+        valence: 0,
+        loudness: 0,
+        speechiness: 0,
+        instrumentalness: 0,
+        liveness: 0,
+        tempo: 0,
+        popularity: 0,
+        key_mode: null,
+        genres: null
+    }
+
+    let avgFeatures: Features = getAverageFeatures(songs);
+
+    return avgFeatures;
+}
+
+export function GowersDistance(base: Features, B: ModSong, weights: Features, max: Features, min: Features)
+{
+    /**
+     * Similarity Sij = Sum(Weightijk * 1 - (|xi - xj| / (max(r) - min(r)))) / sum of weights
+     */
+    let distance: number = 0;
+    let weightSum: number = 0;
+    for(let i: number = 0; i < FEATURE_LENGTH; i++)
+    {
+        const key = FEATURE_NAME_KEYS[i]; 
+        const weight = weights[key] as number;
+        const baseVal: number = base[key] as number; 
+        const bFeatureValue: number = B.normalized_features[key] as number; 
+        const maxVal: number = max[key] as number;
+        const minVal: number = min[key] as number;
+
+        weightSum += weight;
+        distance +=  weight * (1 - (Math.abs(baseVal - bFeatureValue) / (maxVal - minVal))) 
+    }
+    distance /= weightSum;
+    return distance;
 }
--- spotify.ts
+++ spotify.ts
@@ -1,9 +1,14 @@
 import { fetchJsonFromFile } from "./song.ts";
 import type {Features, Song} from "./song.ts"
 
-import {normalizeSongFeatures} from "./normalize.ts"
+import {findMinAndMax, generateBaseCompareFeatures, getAverageFeatures, getWeights, GowersDistance, normalizeSongFeatures} from "./normalize.ts"
 import type {ModSong} from "./normalize.ts"
 
+// interface Similar
+// {
+//     song: ModSong,
+//     distance: number;
+// }
 
 function main()
 {
@@ -12,11 +17,48 @@ function main()
     // {
         // console.log(song.name);
     // }
+    const normed: ModSong[] = normalizeSongFeatures(songs, songs[0]);
+    // console.log(normed)
+    const averages: Features = getAverageFeatures(normed);
+    //Create my base compare songs. Two random ones.
+    const baseSongs: ModSong[] = [normed[0], normed[1]];
+    // const baseFeat: Features = generateBaseCompareFeatures(baseSongs);
+    let distances: Map<ModSong, number> = new Map();
+    const [min, max]: Features[] = findMinAndMax(songs);
+    for(const song of normed)
+    {
+        for(const baseSong of baseSongs)
+        {
+            const weights: Features = getWeights(normed, baseSong.normalized_features, averages)
+            let tempDist: number = GowersDistance(baseSong.normalized_features, song, weights, min, max)
+            const storedDist: number = distances.get(song) ?? Number.POSITIVE_INFINITY;
+            if(tempDist < storedDist) distances.set(song, tempDist);
+        }
+    }
+
+    Array.from(distances.entries())
+            .sort(([,distanceA], [,distanceB]) => {
+                return distanceA - distanceB;
+            })
+            .forEach(([song, distance]) => {
+                console.log(`Song: ${song.name}, Distance: ${distance}`);
+            })
 
 
-    const normed: ModSong[] = normalizeSongFeatures(songs);
-    console.log(normed)
-
+    //Get top 3 songs similar.
+    // for(const song of normed)
+    // {
+    //     distances.push(
+    //         {
+    //             song: song, distance: GowersDistance(baseFeat, song, weights, min, max)
+    //         });
+    // }
+    // distances.sort((a, b) => a.distance - b.distance)
+    // for(let i = 0; i < distances.length; i++)
+    // {
+    //     if (i < 5) console.log(distances[i].song.name);
+    //     // if(distances[i].song.name == "Lancaster Nights") console.log(distances[i]);
+    // }
 }
 
 main()
